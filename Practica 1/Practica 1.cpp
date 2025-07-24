91bd58f5c875862b5acc13b9ad3a0620360fe90c
QTeofilo <127484726+D-Ort@users.noreply.github.com>
2025-07-24 10:58:19 +0200

    Update Practica 1.cpp
    Adjustment of spacing in comments

--- Practica 1/Practica 1.cpp
+++ Practica 1/Practica 1.cpp
@@ -2,17 +2,17 @@
 /************************************************************************************************************************
 NOMBRE DEL PROYECTO:	Práctica 1
 
-OBJETIVO:				Desarrollar el juego de los "Chinos" con múltiples funciones
+OBJETIVO:		Desarrollar el juego de los "Chinos" con múltiples funciones
 
-AUTOR:					MARIO FERNÁNDEZ FERNÁNDEZ & DAVID ORTEGA LOZANO
+AUTOR:			MARIO FERNÁNDEZ FERNÁNDEZ & DAVID ORTEGA LOZANO
 
-FECHA DE CREACIÓN:		27/09/2022
+FECHA DE CREACIÓN:	27/09/2022
 ************************************************************************************************************************/
 
 //-----------------------------------------------------------------------------------------------------------------------
 //BIBLIOTECAS:
 #include <iostream>							//Se incluye la biblioteca de espacios de nombres
-	using namespace std;					//Se selecciona el espacio de nombres estándar
+	using namespace std;						//Se selecciona el espacio de nombres estándar
 #include <cstdlib>							//Se incluye la biblioteca de creación de números aleatorios
 #include <ctime>							//Se incluye la biblioteca de uso del reloj
 
@@ -52,25 +52,25 @@ int main() {
 
 	//VARIABLES LOCALES: 
 	int nRondas = 0, nMax;					//Variables de la función "setup"
-	int puntosB = 0, puntosJ = 0;			//Variables de marcador
+	int puntosB = 0, puntosJ = 0;				//Variables de marcador
 	bool desempate = false;					//Variable que comprueba el desempate
 
 
-											//Se da la bienvenida y se inicializan los parámetros con la función "setup"
+								//Se da la bienvenida y se inicializan los parámetros con la función "setup"
 	cout << "¡Bienvenido al juego de los chinos!" << endl;
 	nMax = setup(nRondas);					
 											
-											//Se juegan todas las rondas establecidas
+								//Se juegan todas las rondas establecidas
 	desempate = juegoChinos(nRondas, nMax, puntosB, puntosJ, 0); 
 
-	while (desempate) {						//En caso de empate y que el jugador haya solicitado desempatar se juega 
-											//otra ronda hasta que las condiciones anteriores cambien
+	while (desempate) {					//En caso de empate y que el jugador haya solicitado desempatar se juega 
+								//otra ronda hasta que las condiciones anteriores cambien
 		desempate = juegoChinos(1, nMax, puntosB, puntosJ, 1);
 	}
 
-	cout << "¡Hasta la próxima! " << endl;	//Se finaliza el juego con una despedida 
+	cout << "¡Hasta la próxima! " << endl;			//Se finaliza el juego con una despedida 
 
-	return 0;								//Se devuelve "0" para comprobar que todo ha ido bien.
+	return 0;						//Se devuelve "0" para comprobar que todo ha ido bien.
 }
 
 
@@ -81,10 +81,10 @@ int main() {
 
 int generarNumeroAleatorio(int limSuperior, int limInferior) {
 
-	srand(time(NULL));						//Se emplea el valor de la hora de la biblioteca "ctime" como semilla de los 
-											//números aleatorios
+	srand(time(NULL));					//Se emplea el valor de la hora de la biblioteca "ctime" como semilla de los 
+								//números aleatorios
 
-											//Se delimita el valor del número "aleatorio" y se devuelve
+								//Se delimita el valor del número "aleatorio" y se devuelve
 	return limInferior + rand() % (limSuperior + 1 - limInferior); 
 }
 
@@ -94,28 +94,28 @@ int generarNumeroAleatorio(int limSuperior, int limInferior) {
 int setup(int& nRondas) {
 
 	//VARIABLES LOCALES:
-	int piedrasMax;							//Variable que recoge el número max de piedras
+	int piedrasMax;						//Variable que recoge el número max de piedras
 
 
 	cout << "Elige el número total de piedras que tenemos cada uno (máx. 10): ";
-	cin >> piedrasMax;						//Se solicita el número máximo de piedras que el jugador quiere usar:
+	cin >> piedrasMax;					//Se solicita el número máximo de piedras que el jugador quiere usar:
 
-											//Se comprueba que se encuentra entre los límites
+								//Se comprueba que se encuentra entre los límites
 	while (piedrasMax > 10 || piedrasMax < 1) {
 		cout << "Número incorrecto de piedras. Elige el número total de piedras que tenemos cada uno(máx. 10): ";
 		cin >> piedrasMax;
 	}
 
 	cout << "¿Cuántas rondas quieres jugar (máx. 10)? "; 
-	cin >> nRondas;							//Se solicita el número de rondas que el jugador quiere jugar:
+	cin >> nRondas;						//Se solicita el número de rondas que el jugador quiere jugar:
 
-											//Se comprueba que se encuentra entre los límites
+								//Se comprueba que se encuentra entre los límites
 	while (nRondas > 10 || nRondas < 1) {
 		cout << "Número incorrecto de rondas. Elige el número de rondas que quieres jugar(máx 10).";
 		cin >> nRondas;
 	}
 
-	return piedrasMax;						//Se devuelve el nº máximo de piedras 
+	return piedrasMax;					//Se devuelve el nº máximo de piedras 
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -124,19 +124,19 @@ int setup(int& nRondas) {
 int jugada(int nMax) {
 
 	//VARIABLES LOCALES:
-	int nPiedras;							//Variable que recoge el nº de piedras del jugador 
+	int nPiedras;						//Variable que recoge el nº de piedras del jugador 
 
 
 	cout << "Tu turno. Con cuántas piedras juegas? Elige un número entre 0 y " << nMax << ": ";
-	cin >> nPiedras;						//Se solicita cuantas piedras quiere el jugador
+	cin >> nPiedras;					//Se solicita cuantas piedras quiere el jugador
 
-											//Se comprueba que se encuentra entre los límites
+								//Se comprueba que se encuentra entre los límites
 	while (nPiedras > nMax || nPiedras < 0) {
 		cout << "Número correcto de piedras, por favor introduzca un número entre 0 y " << nMax << ": ";
 		cin >> nPiedras;
 	}
 
-	return nPiedras;						//Se devuelve el nº de piedras del jugador
+	return nPiedras;					//Se devuelve el nº de piedras del jugador
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -146,7 +146,7 @@ int jugadaBot(int nMax) {
 	//Hemos empleado esta función para organizar mejor el código y así poder ver mejor en que consiste la jugada de la 
 	//Máquina.
 	 
-	return generarNumeroAleatorio(nMax, 0);	//Se devuelvo el nº de piedras de la máquina
+	return generarNumeroAleatorio(nMax, 0);			//Se devuelvo el nº de piedras de la máquina
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -154,14 +154,14 @@ int jugadaBot(int nMax) {
 
 int apuesta(int piedrasJ, int nMax) {
 	//VARIABLES LOCALES:
-	int apuesta;							//Variable que recoge el nº de piedras que apuesta el jugador
+	int apuesta;						//Variable que recoge el nº de piedras que apuesta el jugador
 
 
-	cout << "¿Cuántas piedras hay? ";		//Se solicita el número estimado de piedras que el jugador quiere apostar
+	cout << "¿Cuántas piedras hay? ";			//Se solicita el número estimado de piedras que el jugador quiere apostar
 	cin >> apuesta;
 
 	while (apuesta > (piedrasJ + nMax) || apuesta < piedrasJ) { 
-		if (apuesta < piedrasJ) {			//Se comprueba que se encuentra dentro de los límites:
+		if (apuesta < piedrasJ) {		//Se comprueba que se encuentra dentro de los límites:
 			cout << "Ey, no hagas trampa, apuesta de nuevo:";
 		}
 		else {
@@ -170,7 +170,7 @@ int apuesta(int piedrasJ, int nMax) {
 		cin >> apuesta;
 	}
 
-	return apuesta;							//Se devuelve la apuesta del jugador
+	return apuesta;						//Se devuelve la apuesta del jugador
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -180,8 +180,8 @@ int apuestaBot(int piedrasBot, int nMax) {
 	//Hemos empleado esta función para organizar mejor el código y así poder ver
 	//mejor en que consiste la apuesta de la Máquina.
 	
-											//Se devuelve como apuesta de la máquina la suma de un nº aleatorio < que el 
-											//nº max de piedras y las piedras de la mano de la Máquina
+								//Se devuelve como apuesta de la máquina la suma de un nº aleatorio < que el 
+								//nº max de piedras y las piedras de la mano de la Máquina
 	return piedrasBot + generarNumeroAleatorio(nMax, 0);
 }
 
@@ -193,17 +193,17 @@ bool juegoChinos(int nRondas, int nMax, int& puntosB, int& puntosJ, int desempat
 	//desempate para poder hacer rondas de desempate
 	
 	//VARIABLES LOCALES:
-	int piedrasJ;							//Variable que recoge el nº de piedras que tiene el jugador
-	int piedrasBot;							//Varaible que recoge el nº de piedras que tiene la Máquina 
-	int piedrasTot;							//Variable que recoge el total de piedras empleadas
-	int apuestaJ;							//Variable que recoge la apuesta del jugador
-	int apuestaB;							//Variable que recoge la apuesta de la Máquina
-	int ganadorR;							//Variable que recoge quien ha ganado la ronda
+	int piedrasJ;						//Variable que recoge el nº de piedras que tiene el jugador
+	int piedrasBot;						//Varaible que recoge el nº de piedras que tiene la Máquina 
+	int piedrasTot;						//Variable que recoge el total de piedras empleadas
+	int apuestaJ;						//Variable que recoge la apuesta del jugador
+	int apuestaB;						//Variable que recoge la apuesta de la Máquina
+	int ganadorR;						//Variable que recoge quien ha ganado la ronda
 
 
 	for (int ronda = 1; ronda <= nRondas; ronda++) {
-											//Se repite el juego tantas veces como rondas haya
-		cout << "Ronda ";					//Se dice en que ronda se está y en caso de desempate lo especifico
+								//Se repite el juego tantas veces como rondas haya
+		cout << "Ronda ";				//Se dice en que ronda se está y en caso de desempate lo especifico
 		if (desempatando == 1) {
 			cout << " de desempate" << endl;
 		}
@@ -218,54 +218,54 @@ bool juegoChinos(int nRondas, int nMax, int& puntosB, int& puntosJ, int desempat
 		if (ronda % 2 == 1) {				//En las rondas impares empieza el jugador haciendo la apuesta
 			apuestaJ = apuesta(piedrasJ, nMax);
 
-			do {							//Se comprueba que la apuesta de la Máquina no coincida con la del jugador
+			do {					//Se comprueba que la apuesta de la Máquina no coincida con la del jugador
 				apuestaB = apuestaBot(piedrasBot, nMax);
 			} while (apuestaB == apuestaJ);
 
 			cout << "Yo creo que hay " << apuestaB << " en total." << endl;
 
 		}
-		else {								//En las rondas pares empieza la Máquina haciendo la apuesta
+		else {						//En las rondas pares empieza la Máquina haciendo la apuesta
 			apuestaB = apuestaBot(piedrasBot, nMax);
 			cout << "Yo creo que hay " << apuestaB << " en total." << endl;
 
-			do {							//Se comprueba que la apuesta del jugador no coincida con la de la Máquina
+			do {					//Se comprueba que la apuesta del jugador no coincida con la de la Máquina
 				apuestaJ = apuesta(piedrasJ, nMax);
 			} while (apuestaB == apuestaJ);
 		}
-											//Se muestran ambas apuestas y el número de piedras total
+								//Se muestran ambas apuestas y el número de piedras total
 		cout << "Tú has apostado: " << piedrasJ << ", y yo : " << piedrasBot << " y en total eran " << piedrasTot << endl;
 
-											//Se llama a la función que anuncia el ganador
+								//Se llama a la función que anuncia el ganador
 		ganador(apuestaJ, apuestaB, piedrasTot, puntosJ, puntosB); 
 	}
 	
-	while (puntosJ == puntosB) {			//En caso de empate se propone una ronda de desempate
+	while (puntosJ == puntosB) {				//En caso de empate se propone una ronda de desempate
 		cout << "Hemos empatado, ¿quieres una ronda de desempate? " << endl << "0.- No " << endl << "1.- Sí" << endl;
 		cin >> desempatando;
 
 		if (desempatando == 0) {			//Si no quiere desempatar simplemente se pone fin al juego
 			return false;
 		}
-		else if (desempatando == 1) {		//Si es al contrario, se comenzará una ronda extra de desempate
+		else if (desempatando == 1) {			//Si es al contrario, se comenzará una ronda extra de desempate
 			return true;
 		}
-		else {								//Si se selecciona una opción no válida, se envía mensaje de error
+		else {						//Si se selecciona una opción no válida, se envía mensaje de error
 			cout << "Eso no es un input correcto. " << endl;
 		}
 	}
-											//En caso que haya un ganador se anuncia y se finaliza el juego
+								//En caso que haya un ganador se anuncia y se finaliza el juego
 	cout << "Fin del juego. Resultados: " << endl; 
 	cout << "Ganadas por ti: " << puntosJ << endl;
 	cout << "Ganadas por mi: " << puntosB << endl;
 
 	if (puntosJ > puntosB) {				//Si gana el jugador:
 		cout << "¡ENHORABUENA! Has ganado el juego :) " << endl;
-	}										//Si gana la Máquina:
+	}							//Si gana la Máquina:
 	else if (puntosB > puntosJ) {
 		cout << "Has perdido el juego :(" << endl;
 	}
-	return false;							//Se devuelve false para que termine el juego.
+	return false;						//Se devuelve false para que termine el juego.
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -283,7 +283,7 @@ void ganador(int totalJ, int totalM, int total, int& puntosJ, int& puntosBot) {
 			cout << "Gano la ronda" << endl;
 			puntosBot += 1;
 		}
-		else {								//En caso de empate, se anuncia y no se le suma punto a nadie
+		else {						//En caso de empate, se anuncia y no se le suma punto a nadie
 			cout << "Nadie ha acertado " << endl;
 		}
 }
